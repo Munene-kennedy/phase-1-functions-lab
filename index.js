09ccbc6b10cae2445e12f9744debc1f1c56b012c
Kennedy-munene <kennedymunene309@gmail.com>
2022-12-05 15:12:05 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+const returnfirstTwoDrivers = function (drivers) {
+    const firstTwoDrivers = drivers.slice(0, 2);
+    return firstTwoDrivers;
+  };
+
+  const returnlastTwoDrivers = function (drivers) {
+    const lastTwoDrivers = drivers.slice(-2);
+    return lastTwoDrivers;
+  };
+
+  const selectingDrivers = [returnfirstTwoDrivers , returnlastTwoDrivers]
+
+  const createFareMultiplier = function (multiplier) {
+    return function (value) {
+      return value * multiplier;
+    };
+  };
+
+  const doubleFare = createFareMultiplier(2);
+
+  const tripleFare = createFareMultiplier(3);
+
+  const selectDifferentDrivers = function (drivers, fn) {
+    return fn(drivers);
+  };
